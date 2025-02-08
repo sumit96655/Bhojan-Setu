b6df6ae22a7173191059914811d8b47c8aa22149
sumit96655 <sdmhaske_b22@it.vjti.ac.in>
2025-02-08 12:59:55 +0530

    authentication login signup completed

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSechmea= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true
+    },
+    role:{
+        type:String,
+        enum:['admin',"user","ngo","donor"],
+        default:"user"
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    contact:{
+        type:String,
+        required:true
+    },
+},{timestamps:true})
+
+
+const UserModel= mongoose.model('users',userSechmea)
+
+
+export default UserModel

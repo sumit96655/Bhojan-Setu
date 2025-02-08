a9f96cad70cbbf5da74f816b80613e2cf23772ff
sumit96655 <sdmhaske_b22@it.vjti.ac.in>
2025-02-08 22:44:35 +0530

    backend routes for other functionalities added

--- server/models/Request.js
+++ server/models/Request.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 const requestSchema = new mongoose.Schema({
-    _id: ObjectId,
-    donationId: { type: ObjectId, ref: 'Donation', required: true },
-    ngoId: { type: ObjectId, ref: 'User' },  // for NGO making the claim
-    volunteerId: { type: ObjectId, ref: 'User' }, // if a volunteer is assigned
+    donationId: { type: mongoose.Schema.Types.ObjectId, ref: 'donations', required: true },
+    ngoId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },  // for NGO making the claim
+    volunteerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // if a volunteer is assigned
     requestStatus: { type: String, enum: ['requested', 'approved', 'in-transit', 'completed'], default: 'requested' },
-}
-    , { timestamps: true })
+}, { timestamps: true });
 
-const RequestModel = mongoose.model('request', requestSchema)
+const RequestModel = mongoose.model('request', requestSchema);
 
 export default RequestModel;
